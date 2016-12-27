0d18277fd591c98c6a155b1c045ec5a2fe7265d2
Dorian Morgan <doorain@gmail.com>
2016-12-27 12:18:03 -0700

    christmas break

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -2,10 +2,30 @@ angular.module('lpr', ['ui.router', 'ngAnimate', 'ngTouch'])
 .config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
   $stateProvider
+    .state('home', {
+      url: "/" ,
+      controller:  'homeCtrl',
+      templateUrl: '../src/views/home.html'
+    })
+    .state('chemicalServices', {
+      url: "/chemServices" ,
+      controller:  'chemServicesCtrl',
+      templateUrl: '../src/views/chemServices.html'
+    })
+    .state('burnerMgmt', {
+      url: "/burnerMgmt" ,
+      controller:  'burnerMgmtCtrl',
+      templateUrl: '../src/views/burnerMgmt.html'
+    })
+    .state('serviceRepair', {
+      url: "/serviceRepair" ,
+      controller:  'serviceRepairCtrl',
+      templateUrl: '../src/views/serviceRepair.html'
+    })
+    .state('getQuote', {
+      url: "/getQuote" ,
+      controller:  'getQuoteCtrl',
+      templateUrl: '../src/views/getQuote.html'
+    })
 
-  .state('home', {
-    url: "/" ,
-    controller:  'homeCtrl',
-    templateUrl: '../src/views/home.html'
-  })
 })
